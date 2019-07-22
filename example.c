cc544d0e6a8fa5313e6fe19f1d88d9d0e4d388cb
SG_Muwa <motherlode.muwa@gmail.com>
2019-07-22 22:05:14 +0400

    Add test to example.

--- example.c
+++ example.c
@@ -1,21 +1,29 @@
 /* Simple example of using MINCTEST to test a couple obvious cases. */
 
 #include "minctest.h"
+#include <limits.h>
 
 void test1() {
-    lok('a' == 'a');
+	for(size_t i = 0; i < 4092; i++)
+		lok('a' == 'a');
 }
 
 void test2() {
-    lequal(5, 5);
-    lfequal(5.5, 5.5);
-    lsequal("abc", "abc");
+	ltests += SIZE_MAX / 2;
+	for (size_t i = SIZE_MAX - 10; i < SIZE_MAX; i++)
+	{
+		lequal(5, 5);
+		lfequal(5.5, 5.5);
+		lsequal("abc", "abc");
+		lsequal("a", "b");
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    lrun("test1", test1);
-    lrun("test2", test2);
+    lrun("Basic equalitypass", test1);
+    lrun("Indexing", test2);
     lresults();
+	scanf_s("%*s");
     return lfails != 0;
 }
